c657acd6a331459d2515b8677545c2a89e39fc0e
Dkishore1404 <123447168+Dkishore1404@users.noreply.github.com>
2024-09-24 09:56:46 +0530

    Add files via upload

---
+++ 24.Recognize Dialog Acts.py
@@ -0,0 +1,11 @@
+import nltk
+nltk.download('punkt')
+from nltk import word_tokenize
+def classify_dialog(text):
+    words = set(word_tokenize(text.lower()))
+    if "hello" in words:
+        return "Greeting"
+    elif "how" in words:
+        return "Question"
+    return "Statement"
+print(classify_dialog("Hello, how are you?"))
